9e838f46dad5fa10237deb93442ebddde44e98c7
Felipe Rosa <flp.far@gmail.com>
2020-10-22 09:48:53 -0300

    Add within scope to Store

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,11 @@ class Store < ApplicationRecord
   has_many :ratings
   validates_presence_of :lonlat, :name, :google_place_id
   validates :google_place_id, uniqueness: true
+
+  scope :within, lambda { |longitude, latitude, distance_in_km = 5|
+    where(
+      format("ST_Distance(lonlat, 'POINT(%<lon>f %<lat>f)') < %<dist>d",
+             lon: longitude, lat: latitude, dist: distance_in_km * 1000)
+    )
+  }
 end
